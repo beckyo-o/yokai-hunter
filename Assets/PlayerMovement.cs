6e32236ae07a01f9df02c9bfb524d77280752bdc
Becky Xie <uahtp1234@gmail.com>
2025-07-20 08:46:50 -0400

    First

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtagMovement : MonoBehaviour
+{
+    public float MovementSpeed;
+    public Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        ProcessInputs();
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = movement * MovementSpeed;
+    }
+}
